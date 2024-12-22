44bfb328d154bf82979df49662eafcb173e587a5
panditdhamdhere <dhamdherepandit@gmail.com>
2024-12-22 15:37:13 +0530

    convex database

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+        {
+            domain: "https://thankful-chipmunk-54.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import { SignedIn, SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs";
+
+
+import { SignIn, SignInButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <SignedOut>
-        <SignUpButton />
-      </SignedOut>
-
-      <SignedIn>
-      <SignOutButton />
-      </SignedIn>
-    </div>
+   <div>
+   
+   </div>
   );
 }
